2f5549c0e4133bced2df6fb2a4c383bc993da8a2
Adrià Rocha Coll <arocha18975@iesjoanramis.org>
2024-11-14 19:09:08 +0100

    Primer guardado (paso 3)

---
+++ src/ejerciciogit/EjercicioGit3pt2.java
@@ -0,0 +1,40 @@
+package ejerciciogit;
+
+import java.util.Scanner;
+
+public class EjercicioGit3pt2 {
+	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Calculadora Simple:"); // Nombre
+		
+		System.out.print("Introduce el primer número: "); // Pregunta suma
+		double numero1 = scanner.nextDouble();      
+		
+		System.out.print("Introduce el segundo número: "); // Pregunta resta
+		double numero2 = scanner.nextDouble();   
+		
+		double resultadoSuma = sumar(numero1, numero2);
+		double resultadoResta = restar(numero1, numero2);
+		
+		System.out.println("Resultado de la suma: " + resultadoSuma); // Resultado suma
+		
+		System.out.println("Resultado de la resta: " + resultadoResta); // Resultado resta
+		
+		scanner.close();
+		
+	}
+	
+	public static double sumar(double a, double b) {
+		return a + b;
+	}
+		
+	public static double restar(double a, double b) {
+		return a - b;
+	}
+		
+
+
+}
